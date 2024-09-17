9e5102686624b2638314112f1c5cc797dabe7a1e
Henryekklesia <ekklesiamedialive@gmail.com>
2024-09-17 06:04:09 +0100

    correction on print list integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,12 +1,4 @@
-#!/bin/python3
-
+#!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """Prints all intergers of a list
-
-    Args:
-      my_list: the list argument whose items will be printed
-    """
-
-    # Iterate through list
-    for integer in my_list:
-        print("{:d}".format(integer))
+    for number in my_list:
+        print("{:d}".format(number))
